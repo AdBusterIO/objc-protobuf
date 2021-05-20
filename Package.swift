705564c506f08b0355b1fd32e419ec592a9ba33c
Aleksandr Zhuhan <i@ky1vstar.dev>
2021-05-20 12:25:48 +0300

    Revert "Add JSON format (https://github.com/protocolbuffers/protobuf/pull/4808)"
    This reverts commit bed998011526fe919c3af14d08f569b2bdbce838.

--- Package.swift
+++ Package.swift
@@ -8,10 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "Protobuf",
-            targets: [
-                "Protobuf",
-                "Protobuf+JSONFormat",
-            ]
+            targets: ["Protobuf"]
         ),
     ],
     targets: [
@@ -28,12 +25,6 @@ let package = Package(
                 "GPBProtocolBuffers.m",
             ],
             cSettings: [.unsafeFlags(["-fno-objc-arc"])]
-        ),
-        .target(
-            name: "Protobuf+JSONFormat",
-            dependencies: ["Protobuf"],
-            path: "objectivec+jsonformat",
-            publicHeadersPath: ""
         ),
     ]
 )
